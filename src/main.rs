4289a8dac18cf55d7e71789e0f0dd637ac9e9603
soir20 <71418127+soir20@users.noreply.github.com>
2024-09-22 21:51:03 -0400

    Add const for max buffer size

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crossbeam_channel::{bounded, Receiver, Sender};
 use parking_lot::RwLock;
+use protocol::BufferSize;
 use std::net::{SocketAddr, UdpSocket};
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
@@ -51,7 +52,7 @@ async fn main() {
         &channel_manager_arc,
         &socket_arc,
         client_enqueue.clone(),
-        200,
+        MAX_BUFFER_SIZE,
         1000,
         5,
     );
@@ -71,6 +72,8 @@ async fn main() {
     }
 }
 
+const MAX_BUFFER_SIZE: BufferSize = 512;
+
 struct ServerOptions {
     pub receive_threads: u16,
     pub process_threads: u16,
@@ -82,7 +85,7 @@ fn spawn_receive_threads(
     channel_manager: &Arc<RwLock<ChannelManager>>,
     socket: &Arc<UdpSocket>,
     client_enqueue: Sender<SocketAddr>,
-    initial_buffer_size: u32,
+    initial_buffer_size: BufferSize,
     recency_limit: u16,
     millis_until_resend: u128,
 ) -> Vec<JoinHandle<()>> {
@@ -93,7 +96,7 @@ fn spawn_receive_threads(
             let client_enqueue = client_enqueue.clone();
 
             thread::spawn(move || loop {
-                let mut buf = [0; 512];
+                let mut buf = [0; MAX_BUFFER_SIZE as usize];
                 if let Ok((len, src)) = socket.recv_from(&mut buf) {
                     let recv_data = &buf[0..len];
 
