c271b657669dedcfe454e31b7c0f9789fe4888f6
soir20 <71418127+soir20@users.noreply.github.com>
2024-11-22 01:01:07 -0500

    Update second anim defaults

--- src/game_server/handlers/housing.rs
+++ src/game_server/handlers/housing.rs
@@ -192,7 +192,7 @@ pub fn fixture_packets(
                 unknown26: true,
                 ignore_position: true,
                 sub_title_id: 0,
-                one_shot_animation_id: 0,
+                one_shot_animation_id: -1,
                 temporary_appearance: 0,
                 effects: vec![],
                 disable_interact_popup: false,
