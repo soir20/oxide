3a1311aa9586bf3eb30fe207cb9563de06302c2c
soir20 <71418127+soir20@users.noreply.github.com>
2024-12-15 22:52:07 -0500

    Fix array type

--- src/game_server/packets/mod.rs
+++ src/game_server/packets/mod.rs
@@ -422,6 +422,6 @@ pub struct RewardBundle {
     pub unknown13: u32,
     pub unknown14: u32,
     pub unknown15: u32,
-    pub unknown16: Vec<u32>,
+    pub unknown16: Vec<RewardEntry>,
     pub unknown17: u32,
 }
