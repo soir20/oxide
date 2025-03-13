62623c78fcfb6f2752bbbc292436ce67f29cab9d
soir20 <71418127+soir20@users.noreply.github.com>
2025-03-13 02:14:13 -0400

    Add Saber Strike end game packets

--- src/game_server/packets/saber_strike.rs
+++ src/game_server/packets/saber_strike.rs
@@ -10,6 +10,8 @@ use super::{
 #[repr(i32)]
 pub enum SaberStrikeOpCode {
     StageData = 0x1,
+    GameOver = 0x2,
+    ObfuscatedScore = 0x9,
 }
 
 #[derive(SerializePacket, DeserializePacket)]
@@ -23,4 +25,45 @@ impl GamePacket for SaberStrikeStageData {
     type Header = MinigameOpCode;
 
     const HEADER: Self::Header = MinigameOpCode::SaberStrike;
+}
+
+#[derive(SerializePacket, DeserializePacket)]
+pub struct SaberStrikeGameOver {
+    pub won: bool,
+    pub score: u32,
+    pub best_throw: u32,
+    pub enemies_killed: u32,
+    pub duration_seconds: f32,
+    pub remaining_sabers: u32,
+}
+
+impl GamePacket for SaberStrikeGameOver {
+    type Header = MinigameOpCode;
+
+    const HEADER: Self::Header = MinigameOpCode::SaberStrike;
+}
+
+fn ror(x: u32, bits: u8) -> u32 {
+    let right_shift = (bits as u32) % u32::BITS;
+    let left_shift = 32 - right_shift;
+    (x >> right_shift) | (x << left_shift)
+}
+
+#[derive(SerializePacket, DeserializePacket)]
+pub struct SaberStrikeObfuscatedScore {
+    pub minigame_header: MinigameHeader,
+    seed: u32,
+    obfuscated_score: u32,
+}
+
+impl GamePacket for SaberStrikeObfuscatedScore {
+    type Header = MinigameOpCode;
+
+    const HEADER: Self::Header = MinigameOpCode::SaberStrike;
+}
+
+impl SaberStrikeObfuscatedScore {
+    pub fn score(&self) -> u32 {
+        self.obfuscated_score ^ self.seed - ror(self.seed, 1)
+    }
 }
