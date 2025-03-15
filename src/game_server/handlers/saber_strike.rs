712c96c06834970c5d9e05b79a685de88a954a6e
soir20 <71418127+soir20@users.noreply.github.com>
2025-03-15 00:50:40 -0400

    Move end game handling to separate function

--- src/game_server/handlers/saber_strike.rs
+++ src/game_server/handlers/saber_strike.rs
@@ -25,76 +25,7 @@ pub fn process_saber_strike_packet(
         Ok(op_code) => match op_code {
             SaberStrikeOpCode::GameOver => {
                 let game_over = SaberStrikeGameOver::deserialize(cursor)?;
-                handle_minigame_packet_write(
-                    sender,
-                    game_server,
-                    &header,
-                    |minigame_status, minigame_stats, _, stage_config| {
-                        minigame_status.score_entries.push(ScoreEntry {
-                            entry_text: "lt_TotalTime".to_string(),
-                            icon_set_id: 0,
-                            score_type: ScoreType::Time,
-                            score_count: i32::try_from(game_over.duration_seconds.round() as i64)
-                                .unwrap_or_default(),
-                            score_max: 0,
-                            score_points: 0,
-                        });
-                        minigame_status.score_entries.push(ScoreEntry {
-                            entry_text: "lt_ThrowsRemaining".to_string(),
-                            icon_set_id: 0,
-                            score_type: ScoreType::Counter,
-                            score_count: game_over.remaining_sabers,
-                            score_max: 0,
-                            score_points: 0,
-                        });
-                        minigame_status.score_entries.push(ScoreEntry {
-                            entry_text: "lt_TotalDestroyed".to_string(),
-                            icon_set_id: 0,
-                            score_type: ScoreType::Counter,
-                            score_count: game_over.enemies_killed,
-                            score_max: 0,
-                            score_points: 0,
-                        });
-                        minigame_status.score_entries.push(ScoreEntry {
-                            entry_text: "lt_BestThrow".to_string(),
-                            icon_set_id: 0,
-                            score_type: ScoreType::Counter,
-                            score_count: game_over.best_throw,
-                            score_max: 0,
-                            score_points: 0,
-                        });
-                        minigame_status.score_entries.push(ScoreEntry {
-                            entry_text: "lt_TotalScore".to_string(),
-                            icon_set_id: 0,
-                            score_type: ScoreType::Total,
-                            score_count: game_over.score,
-                            score_max: 0,
-                            score_points: 0,
-                        });
-                        minigame_status.total_score = game_over.score;
-                        minigame_status.game_won = game_over.won;
-                        if game_over.won {
-                            minigame_stats
-                                .complete(stage_config.stage_config.guid, game_over.score);
-                        }
-                        Ok(())
-                    },
-                )?;
-
-                game_server.lock_enforcer().write_characters(
-                    |characters_table_write_handle, zones_lock_enforcer| {
-                        zones_lock_enforcer.write_zones(|zones_table_write_handle| {
-                            end_active_minigame(
-                                sender,
-                                characters_table_write_handle,
-                                zones_table_write_handle,
-                                header.stage_guid,
-                                false,
-                                game_server,
-                            )
-                        })
-                    },
-                )
+                handle_saber_strike_game_over(&header, &game_over, sender, game_server)
             }
             _ => {
                 let mut buffer = Vec::new();
@@ -116,4 +47,81 @@ pub fn process_saber_strike_packet(
             Ok(Vec::new())
         }
     }
+}
+
+fn handle_saber_strike_game_over(
+    header: &MinigameHeader,
+    game_over: &SaberStrikeGameOver,
+    sender: u32,
+    game_server: &GameServer,
+) -> Result<Vec<Broadcast>, ProcessPacketError> {
+    handle_minigame_packet_write(
+        sender,
+        game_server,
+        header,
+        |minigame_status, minigame_stats, _, stage_config| {
+            minigame_status.score_entries.push(ScoreEntry {
+                entry_text: "lt_TotalTime".to_string(),
+                icon_set_id: 0,
+                score_type: ScoreType::Time,
+                score_count: i32::try_from(game_over.duration_seconds.round() as i64)
+                    .unwrap_or_default(),
+                score_max: 0,
+                score_points: 0,
+            });
+            minigame_status.score_entries.push(ScoreEntry {
+                entry_text: "lt_ThrowsRemaining".to_string(),
+                icon_set_id: 0,
+                score_type: ScoreType::Counter,
+                score_count: game_over.remaining_sabers,
+                score_max: 0,
+                score_points: 0,
+            });
+            minigame_status.score_entries.push(ScoreEntry {
+                entry_text: "lt_TotalDestroyed".to_string(),
+                icon_set_id: 0,
+                score_type: ScoreType::Counter,
+                score_count: game_over.enemies_killed,
+                score_max: 0,
+                score_points: 0,
+            });
+            minigame_status.score_entries.push(ScoreEntry {
+                entry_text: "lt_BestThrow".to_string(),
+                icon_set_id: 0,
+                score_type: ScoreType::Counter,
+                score_count: game_over.best_throw,
+                score_max: 0,
+                score_points: 0,
+            });
+            minigame_status.score_entries.push(ScoreEntry {
+                entry_text: "lt_TotalScore".to_string(),
+                icon_set_id: 0,
+                score_type: ScoreType::Total,
+                score_count: game_over.score,
+                score_max: 0,
+                score_points: 0,
+            });
+            minigame_status.total_score = game_over.score;
+            minigame_status.game_won = game_over.won;
+            if game_over.won {
+                minigame_stats.complete(stage_config.stage_config.guid, game_over.score);
+            }
+            Ok(())
+        },
+    )?;
+
+    game_server.lock_enforcer().write_characters(
+        |characters_table_write_handle, zones_lock_enforcer| {
+            zones_lock_enforcer.write_zones(|zones_table_write_handle| {
+                end_active_minigame(
+                    sender,
+                    characters_table_write_handle,
+                    zones_table_write_handle,
+                    header.stage_guid,
+                    false,
+                    game_server,
+                )
+            })
+        },
+    )
 }
