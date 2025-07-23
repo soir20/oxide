370d344c116a7cde0c5bb683c648cbe9c9041aca
soir20 <71418127+soir20@users.noreply.github.com>
2025-07-23 01:48:20 -0400

    Fix loading not finishing for non-teleport games

--- src/game_server/handlers/minigame.rs
+++ src/game_server/handlers/minigame.rs
@@ -2703,6 +2703,13 @@ pub fn create_active_minigame_if_uncreated(
                     unknown27: 0,
                 },
             }),
+            GamePacket::serialize(&TunneledPacket {
+                unknown1: true,
+                inner: ExecuteScriptWithStringParams {
+                    script_name: "MinigameHowtoHandler.handleLoadingDone".to_string(),
+                    params: Vec::new(),
+                },
+            }),
         ],
     )])
 }
