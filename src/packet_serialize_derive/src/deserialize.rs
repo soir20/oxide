1b7b10065d5b638abb0dc6af3d42832765ccc642
soir20 <71418127+soir20@users.noreply.github.com>
2025-08-24 17:30:30 -0400

    Support deserializepacket for enums

--- src/packet_serialize_derive/src/deserialize.rs
+++ src/packet_serialize_derive/src/deserialize.rs
@@ -1,9 +1,9 @@
 use proc_macro2::TokenStream;
 use quote::{quote, quote_spanned};
 use syn::spanned::Spanned;
-use syn::{parse_quote, Data, Fields, GenericParam, Generics, Index};
+use syn::{parse_quote, DataStruct, Fields, GenericParam, Generics, Index};
 
-pub fn add_trait_bounds(mut generics: Generics) -> Generics {
+pub fn add_struct_trait_bounds(mut generics: Generics) -> Generics {
     for param in &mut generics.params {
         if let GenericParam::Type(ref mut type_param) = *param {
             type_param
@@ -14,39 +14,55 @@ pub fn add_trait_bounds(mut generics: Generics) -> Generics {
     generics
 }
 
-pub fn assign_fields(data: &Data) -> TokenStream {
-    match *data {
-        Data::Struct(ref data) => match data.fields {
-            Fields::Named(ref fields) => {
-                let assignments = fields.named.iter().map(|f| {
-                    let name = &f.ident;
-                    quote_spanned! {f.span()=>
-                        #name: packet_serialize::DeserializePacket::deserialize(cursor)?,
-                    }
-                });
-                quote! {
-                    #(
-                        #assignments
-                    )*
+pub fn add_enum_trait_bounds(mut generics: Generics) -> Generics {
+    generics = add_struct_trait_bounds(generics);
+    for param in &mut generics.params {
+        if let GenericParam::Type(ref mut type_param) = *param {
+            type_param
+                .bounds
+                .push(parse_quote!(num_enum::TryFromPrimitive));
+        }
+    }
+    generics
+}
+
+pub fn assign_struct_fields(data: &DataStruct) -> TokenStream {
+    match data.fields {
+        Fields::Named(ref fields) => {
+            let assignments = fields.named.iter().map(|f| {
+                let name = &f.ident;
+                quote_spanned! {f.span()=>
+                    #name: packet_serialize::DeserializePacket::deserialize(cursor)?,
                 }
+            });
+            quote! {
+                #(
+                    #assignments
+                )*
             }
-            Fields::Unnamed(ref fields) => {
-                let assignments = fields.unnamed.iter().enumerate().map(|(i, f)| {
-                    let index = Index::from(i);
-                    quote_spanned! {f.span()=>
-                        #index: packet_serialize::DeserializePacket::deserialize(cursor)?,
-                    }
-                });
-                quote! {
-                    #(
-                        #assignments
-                    )*
+        }
+        Fields::Unnamed(ref fields) => {
+            let assignments = fields.unnamed.iter().enumerate().map(|(i, f)| {
+                let index = Index::from(i);
+                quote_spanned! {f.span()=>
+                    #index: packet_serialize::DeserializePacket::deserialize(cursor)?,
                 }
+            });
+            quote! {
+                #(
+                    #assignments
+                )*
             }
-            Fields::Unit => {
-                quote!()
-            }
-        },
-        Data::Enum(_) | Data::Union(_) => unimplemented!(),
+        }
+        Fields::Unit => {
+            quote!()
+        }
+    }
+}
+
+pub fn assign_enum_variant() -> TokenStream {
+    quote! {
+        let primitive = <Self as num_enum::TryFromPrimitive>::Primitive::deserialize(cursor)?;
+        Self::try_from_primitive(primitive).map_err(|_| packet_serialize::DeserializePacketError::UnknownDiscriminator)
     }
 }
--- src/packet_serialize_derive/src/lib.rs
+++ src/packet_serialize_derive/src/lib.rs
@@ -5,6 +5,11 @@ use quote::quote;
 use syn::parse_macro_input;
 use syn::DeriveInput;
 
+use crate::deserialize::add_enum_trait_bounds;
+use crate::deserialize::add_struct_trait_bounds;
+use crate::deserialize::assign_enum_variant;
+use crate::deserialize::assign_struct_fields;
+
 #[proc_macro_derive(SerializePacket)]
 pub fn derive_serialize(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -33,17 +38,28 @@ pub fn derive_deserialize(input: proc_macro::TokenStream) -> proc_macro::TokenSt
 
     let name = input.ident;
 
-    let generics = deserialize::add_trait_bounds(input.generics);
-    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    let (generics, body) = match &input.data {
+        syn::Data::Struct(data) => {
+            let assignments = assign_struct_fields(data);
+            (
+                add_struct_trait_bounds(input.generics),
+                quote! {
+                    Ok(#name {
+                        #assignments
+                    })
+                },
+            )
+        }
+        syn::Data::Enum(_) => (add_enum_trait_bounds(input.generics), assign_enum_variant()),
+        syn::Data::Union(_) => unimplemented!(),
+    };
 
-    let assignments = deserialize::assign_fields(&input.data);
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
     let expanded = quote! {
         impl #impl_generics packet_serialize::DeserializePacket for #name #ty_generics #where_clause {
             fn deserialize(cursor: &mut std::io::Cursor<&[u8]>) -> Result<Self, packet_serialize::DeserializePacketError> {
-                Ok(#name {
-                    #assignments
-                })
+                #body
             }
         }
     };
