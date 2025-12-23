7bd4e51100ec073954dd269a58ef06d47b321e33
soir20 <71418127+soir20@users.noreply.github.com>
2025-12-22 22:43:43 -0500

    Revert "Revert field name to unknown"
    This reverts commit c5a6d6caa3277d1a3a5a972e4fdb388918f2afe8.

--- src/game_server/packets/command.rs
+++ src/game_server/packets/command.rs
@@ -154,7 +154,7 @@ pub struct InteractionList {
     pub guid: u64,
     pub auto_select_if_single_button: bool,
     pub buttons: Vec<InteractionButton>,
-    pub unknown2: String,
+    pub context_name: String,
     pub unknown3: bool,
     pub hidden: bool,
 }
