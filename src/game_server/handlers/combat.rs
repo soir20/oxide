c6d8b30548e2eebf27accc9a3cdd34ec888c2b4b
soir20 <71418127+soir20@users.noreply.github.com>
2026-01-04 19:06:06 -0500

    Add enemy prioritization calculator

--- src/game_server/handlers/combat.rs
+++ src/game_server/handlers/combat.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashSet, fs::File, path::Path};
+use std::{
+    collections::{HashMap, HashSet},
+    fs::File,
+    path::Path,
+};
 
 use crate::ConfigError;
 
@@ -8,4 +12,27 @@ pub fn load_enemy_types(config_dir: &Path) -> Result<HashSet<String>, ConfigErro
     Ok(enemy_types)
 }
 
-pub struct EnemyPrioritization {}
+pub struct EnemyPrioritization {
+    priority_points_by_type: HashMap<String, i8>,
+}
+
+impl EnemyPrioritization {
+    pub fn priority<'a>(&self, enemy_types: impl Iterator<Item = &'a String>) -> i8 {
+        enemy_types.fold(0, |acc, enemy_type| {
+            acc.saturating_add(
+                self.priority_points_by_type
+                    .get(enemy_type)
+                    .copied()
+                    .unwrap_or_default(),
+            )
+        })
+    }
+}
+
+impl From<HashMap<String, i8>> for EnemyPrioritization {
+    fn from(priority_points_by_type: HashMap<String, i8>) -> Self {
+        EnemyPrioritization {
+            priority_points_by_type,
+        }
+    }
+}
