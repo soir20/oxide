17b777a22d26f87cff01fb9ad8657b1efe93e203
soir20 <71418127+soir20@users.noreply.github.com>
2026-01-04 19:58:04 -0500

    Add threat table

--- src/game_server/handlers/combat.rs
+++ src/game_server/handlers/combat.rs
@@ -4,6 +4,8 @@ use std::{
     path::Path,
 };
 
+use mut_binary_heap::BinaryHeap;
+
 use crate::ConfigError;
 
 pub fn load_enemy_types(config_dir: &Path) -> Result<HashSet<String>, ConfigError> {
@@ -33,6 +35,38 @@ impl From<HashMap<String, i8>> for EnemyPrioritization {
     fn from(priority_points_by_type: HashMap<String, i8>) -> Self {
         EnemyPrioritization {
             priority_points_by_type,
+        }
+    }
+}
+
+pub struct ThreatTable {
+    heap: BinaryHeap<u64, (i8, u32)>,
+    prioritization: EnemyPrioritization,
+}
+
+impl ThreatTable {
+    pub fn deal_damage<'a>(
+        &mut self,
+        attacker_guid: u64,
+        attacker_types: impl Iterator<Item = &'a String>,
+        damage_dealt: u32,
+    ) {
+        if let Some(mut value) = self.heap.get_mut(&attacker_guid) {
+            *value = (value.0, value.1.saturating_add(damage_dealt));
+        }
+
+        if !self.heap.contains_key(&attacker_guid) {
+            let priority = self.prioritization.priority(attacker_types);
+            self.heap.push(attacker_guid, (priority, damage_dealt));
+        }
+    }
+}
+
+impl From<HashMap<String, i8>> for ThreatTable {
+    fn from(priority_points_by_type: HashMap<String, i8>) -> Self {
+        ThreatTable {
+            heap: BinaryHeap::new(),
+            prioritization: priority_points_by_type.into(),
         }
     }
 }
