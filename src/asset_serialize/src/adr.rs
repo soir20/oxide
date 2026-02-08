b3d737aa60775ed8363c2616919d22d807f365fe
soir20 <71418127+soir20@users.noreply.github.com>
2026-02-08 02:55:09 -0500

    Add len reader function

--- src/asset_serialize/src/adr.rs
+++ src/asset_serialize/src/adr.rs
@@ -1,26 +1,69 @@
-use std::path::PathBuf;
+use num_enum::TryFromPrimitive;
+use tokio::{
+    fs::File,
+    io::{AsyncReadExt, BufReader},
+};
 
-use tokio::fs::File;
+use crate::{deserialize, deserialize_null_terminated_string, tell, Error, ErrorKind};
 
-use crate::{DeserializeAsset, Error};
+async fn deserialize_len(file: &mut BufReader<&mut File>) -> Result<i32, Error> {
+    let len_marker = deserialize(file, BufReader::read_i8).await?;
+    let mut len: i32 = len_marker.into();
+    if len_marker < 0 {
+        if len_marker == -1 {
+            len = deserialize(file, BufReader::read_i32_le).await?;
+        } else {
+            let len_byte2 = deserialize(file, BufReader::read_i8).await?;
+            len = ((i32::from(len_marker) & 0b0111_1111) << 8) | i32::from(len_byte2);
+        }
+    }
 
-pub enum SkeletonType {
-    FileName = 1,
+    Ok(len)
 }
 
-/*impl DeserializeAsset for SkeletonType {
-    fn deserialize(
-        path: PathBuf,
-        file: &mut File,
-    ) -> impl std::future::Future<Output = Result<Self, Error>> + Send {
-        todo!()
+#[derive(Copy, Clone, Debug, TryFromPrimitive)]
+#[repr(u8)]
+pub enum SkeletonType {
+    AssetName = 1,
+}
+
+impl SkeletonType {
+    async fn deserialize(file: &mut BufReader<&mut File>) -> Result<Self, Error> {
+        let offset = tell(file).await;
+        let value = deserialize(file, BufReader::read_u8).await?;
+        SkeletonType::try_from_primitive(value).map_err(|_| Error {
+            kind: ErrorKind::UnknownDiscriminant(value.into()),
+            offset,
+        })
     }
-}*/
+}
+
+pub enum SkeletonTypeData {
+    AssetName { name: String },
+}
 
 pub struct SkeletonEntry {
     pub skeleton_type: SkeletonType,
-    pub compressed_len: u16,
-    pub decompressed_len: u32,
+    pub len: i32,
+    pub data: SkeletonTypeData,
+}
+
+impl SkeletonEntry {
+    async fn deserialize(file: &mut BufReader<&mut File>) -> Result<Self, Error> {
+        let skeleton_type = SkeletonType::deserialize(file).await?;
+        let len = deserialize_len(file).await?;
+        let data = match skeleton_type {
+            SkeletonType::AssetName => SkeletonTypeData::AssetName {
+                name: deserialize_null_terminated_string(file).await?,
+            },
+        };
+
+        Ok(SkeletonEntry {
+            skeleton_type,
+            len,
+            data,
+        })
+    }
 }
 
 pub struct Adr {}
