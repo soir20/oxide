70539aa203a3dce4c93b5113be1e7570b85bffe4
Delta-07 <166327194+Delta-07@users.noreply.github.com>
2026-02-17 02:25:16 -0600

    Remove presumably unused ability packets

--- src/game_server/packets/ability.rs
+++ src/game_server/packets/ability.rs
@@ -9,18 +9,9 @@ use super::{player_data::AbilityType, ActionBarType, GamePacket, OpCode, Pos, Ta
 #[derive(Copy, Clone, Debug, TryFromPrimitive)]
 #[repr(u16)]
 pub enum AbilityOpCode {
-    AbilityFailed = 0x1,
-    StartCasting = 0x3,
     LaunchAndLand = 0x4,
-    SetDefinition = 0x5,
-    ClientMoveAndCast = 0x6,
-    PurchaseAbility = 0x7,
-    UpdateAbilityExperience = 0x8,
-    StopAura = 0x9,
     RequestStartAbility = 0xa,
-    MeleeRefresh = 0xb,
     RequestAbilityDefinition = 0xc,
-    AbilityDefinition = 0xd,
     DetonateProjectile = 0xe,
     PulseLocationTargeting = 0xf,
     ReceivePulseLocation = 0x10,
@@ -113,31 +104,6 @@ impl GamePacket for RequestStartAbility {
     const HEADER: Self::Header = AbilityOpCode::RequestStartAbility;
 }
 
-#[derive(SerializePacket, DeserializePacket)]
-pub struct AbilityFailed {
-    pub message_id: u32,
-}
-
-impl GamePacket for AbilityFailed {
-    type Header = AbilityOpCode;
-    const HEADER: Self::Header = AbilityOpCode::AbilityFailed;
-}
-
-#[derive(SerializePacket, DeserializePacket)]
-pub struct StartCasting {
-    pub caster_guid: u64,
-    pub target_guid: u64,
-    pub caster_composite_effect_id: u32,
-    pub caster_animation_id: u32,
-    pub ability_id: u32,
-    pub unknown6: Pos,
-}
-
-impl GamePacket for StartCasting {
-    type Header = AbilityOpCode;
-    const HEADER: Self::Header = AbilityOpCode::StartCasting;
-}
-
 #[derive(SerializePacket)]
 pub struct LaunchAndLand {
     pub launcher_guid: u64,
@@ -199,155 +165,6 @@ impl GamePacket for LaunchAndLand {
 }
 
 #[derive(SerializePacket, DeserializePacket)]
-pub struct ClientMoveAndCast {
-    pub unknown1: Pos,
-    pub unknown2: u32,
-    pub unknown3: u32,
-}
-
-impl GamePacket for ClientMoveAndCast {
-    type Header = AbilityOpCode;
-    const HEADER: Self::Header = AbilityOpCode::ClientMoveAndCast;
-}
-
-#[derive(SerializePacket, DeserializePacket)]
-pub struct UpdateAbilityExperience {
-    pub unknown1: u32,
-    pub unknown2: u32,
-    pub unknown3: u32,
-    pub unknown4: u32,
-    pub unknown5: u32,
-    pub unknown6: u32,
-}
-
-impl GamePacket for UpdateAbilityExperience {
-    type Header = AbilityOpCode;
-    const HEADER: Self::Header = AbilityOpCode::UpdateAbilityExperience;
-}
-
-#[derive(SerializePacket, DeserializePacket)]
-pub struct StopAura {
-    pub unknown1: u32,
-    pub unknown2: u32,
-}
-
-impl GamePacket for StopAura {
-    type Header = AbilityOpCode;
-    const HEADER: Self::Header = AbilityOpCode::StopAura;
-}
-
-#[derive(SerializePacket, DeserializePacket)]
-pub struct MeleeRefresh {
-    pub refresh_millis: u32,
-}
-
-impl GamePacket for MeleeRefresh {
-    type Header = AbilityOpCode;
-    const HEADER: Self::Header = AbilityOpCode::MeleeRefresh;
-}
-
-#[derive(SerializePacket, DeserializePacket)]
-pub struct UnknownAbilityDefArray {
-    pub unknown1: u32,
-    pub unknown2: u32,
-    pub unknown3: u32,
-    pub ability_id: u32,
-    pub unknown5: u32,
-    pub unknown6: u32,
-    pub unknown7: bool,
-    pub unknown8: u32,
-    pub unknown9: u32,
-    pub unknown10: f32,
-    pub unknown11: f32,
-    pub unknown12: f32,
-    pub unknown13: u32,
-    pub unknown14: u32,
-    pub unknown15: u32,
-    pub unknown16: u32,
-    pub unknown17: u32,
-    pub unknown18: u32,
-    pub unknown19: bool,
-    pub unknown20: bool,
-    pub unknown21: String,
-    pub unknown22: f32,
-    pub unknown23: f32,
-    pub unknown24: f32,
-    pub unknown25: f32,
-    pub unknown26: f32,
-    pub unknown27: f32,
-    pub unknown28: u32,
-    pub unknown29: u32,
-    pub unknown30: u32,
-    pub unknown31: f32,
-    pub unknown32: u32,
-    pub unknown33: u32,
-    pub unknown34: u32,
-    pub unknown35: u32,
-    pub unknown36: u32,
-    pub unknown37: u32,
-    pub unknown38: u32,
-    pub unknown39: u32,
-    pub unknown40: bool,
-    pub unknown41: f32,
-    pub unknown42: f32,
-    pub unknown43: f32,
-    pub unknown44: bool,
-}
-
-#[derive(SerializePacket, DeserializePacket)]
-pub struct AbilityDefinition {
-    pub ability_id: u32,
-    pub unknown2: bool,
-    pub unknown3: bool,
-    pub name_id: u32,
-    pub description: u32,
-    pub unknown6: u32,
-    pub unknown7: f32,
-    pub unknown8: u32,
-    pub destination_land_composite_effect_id: u32, // unconfirmed
-    pub target_land_composite_effect_id: u32,      // uncomfirmed
-    pub unknown11: u32,
-    pub cast_animation_id: u32,
-    pub land_animation_id: u32,
-    pub unknown14: u32,
-    pub unknown15: u32,
-    pub unknown16: u32,
-    pub unknown17: f32,
-    pub unknown18: f32,
-    pub unknown19: u32,
-    pub unknown20: u32,
-    pub unknown21: u32,
-    pub unknown22: bool,
-    pub unknown23: u32,
-    pub unknown24: u32,
-    pub unknown25: u32,
-    pub unknown26: f32,
-    pub trail_composite_effect_id: u32,
-    pub description2: u32,
-    pub unknown29: f32,
-    pub unknown30: f32,
-    pub unknown31: u32,
-    pub unknown32: u32,
-    pub unknown33: u32,
-    pub unknown34: f32,
-    pub unknown35: f32,
-    pub unknown36: bool,
-    pub unknown37: u32,
-    pub unknown38: u32,
-    pub unknown39: bool,
-    pub unknown40: bool,
-    pub unknown41: bool,
-    pub unknown42: u32,
-    pub unknown43: f32,
-    pub unknown44: Vec<UnknownAbilityDefArray>,
-}
-
-impl GamePacket for AbilityDefinition {
-    type Header = AbilityOpCode;
-    const HEADER: Self::Header = AbilityOpCode::AbilityDefinition;
-}
-
-#[derive(SerializePacket, DeserializePacket)]
 pub struct DetonateProjectile {
     pub guid: u64,
     pub animation_id: u32,
@@ -369,14 +186,4 @@ pub struct PulseLocationTargeting {
 impl GamePacket for PulseLocationTargeting {
     type Header = AbilityOpCode;
     const HEADER: Self::Header = AbilityOpCode::PulseLocationTargeting;
-}
-
-#[derive(SerializePacket)]
-pub struct AbilitySetDefinition {
-    pub abilities: Vec<AbilityType>,
-}
-
-impl GamePacket for AbilitySetDefinition {
-    type Header = AbilityOpCode;
-    const HEADER: Self::Header = AbilityOpCode::SetDefinition;
 }
