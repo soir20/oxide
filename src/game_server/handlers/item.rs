0e6912c0d43af94196cd3119eb82687086291b5d
Delta-07 <166327194+Delta-07@users.noreply.github.com>
2026-02-20 02:50:53 -0600

    Support multiple item YAMLs and subfolders

--- src/game_server/handlers/item.rs
+++ src/game_server/handlers/item.rs
@@ -1,4 +1,9 @@
-use std::{collections::BTreeMap, fs::File, path::Path};
+use std::{
+    collections::BTreeMap,
+    fs,
+    fs::File,
+    path::{Path, PathBuf},
+};
 
 use crate::{game_server::packets::item::ItemDefinition, ConfigError};
 
@@ -7,14 +12,40 @@ pub const SABER_ITEM_TYPE: u32 = 25;
 pub fn load_item_definitions(
     config_dir: &Path,
 ) -> Result<BTreeMap<u32, ItemDefinition>, ConfigError> {
-    let mut file = File::open(config_dir.join("items.yaml"))?;
-    let item_defs: Vec<ItemDefinition> = serde_yaml::from_reader(&mut file)?;
+    let items_dir = config_dir.join("items");
 
-    let mut item_def_map = BTreeMap::new();
-    for item_def in item_defs {
-        if let Some(previous_item_def) = item_def_map.insert(item_def.guid, item_def) {
-            panic!("Two item definitions have ID {}", previous_item_def.guid);
+    fn find_yaml_files(root: &Path) -> Result<Vec<PathBuf>, ConfigError> {
+        let mut files = Vec::new();
+
+        for entry in fs::read_dir(root)? {
+            let entry_path = entry?.path();
+
+            if entry_path.is_dir() {
+                files.extend(find_yaml_files(&entry_path)?);
+            } else if entry_path.extension().is_some_and(|ext| ext == "yaml") {
+                files.push(entry_path);
+            }
+        }
+
+        Ok(files)
+    }
+
+    let yaml_files = find_yaml_files(&items_dir)?;
+    let mut items = BTreeMap::new();
+
+    for file_path in yaml_files {
+        let file = File::open(&file_path)?;
+        let item_defs: Vec<ItemDefinition> = serde_yaml::from_reader(file)?;
+
+        for item_def in item_defs {
+            if let Some(previous) = items.insert(item_def.guid, item_def) {
+                return Err(ConfigError::ConstraintViolated(format!(
+                    "Two item definitions have ID {} (file: {:?})",
+                    previous.guid, file_path
+                )));
+            }
         }
     }
-    Ok(item_def_map)
+
+    Ok(items)
 }
