d2ccdeb60e7f9132f575edebd8422cbd914a9c36
Delta-07 <166327194+Delta-07@users.noreply.github.com>
2026-02-27 01:15:20 -0600

    Remove redundant fields

--- src/game_server/handlers/item.rs
+++ src/game_server/handlers/item.rs
@@ -16,6 +16,14 @@ use serde::Deserialize;
 
 pub const SABER_ITEM_TYPE: u32 = 25;
 
+const fn default_item_class() -> i32 {
+    -1
+}
+
+const fn default_stack_size() -> i32 {
+    1
+}
+
 #[derive(Debug, Deserialize)]
 pub struct ItemAbilityConfig {
     pub ability_icon: u32,
@@ -25,37 +33,62 @@ pub struct ItemAbilityConfig {
 #[derive(Debug, Deserialize)]
 pub struct ItemConfig {
     pub guid: u32,
+    #[serde(default)]
     pub name_id: u32,
+    #[serde(default)]
     pub description_id: u32,
+    #[serde(default)]
     pub icon_set_id: u32,
+    #[serde(default)]
     pub tint: u32,
+    #[serde(default)]
     pub cost: u32,
+    #[serde(default = "default_item_class")]
     pub item_class: i32,
+    #[serde(default)]
     pub required_battle_class: u32,
     pub slot: ItemType,
+    #[serde(default)]
     pub disable_trade: bool,
+    #[serde(default)]
     pub disable_sale: bool,
+    #[serde(default)]
     pub model_name: String,
+    #[serde(default)]
     pub texture_alias: String,
+    #[serde(default)]
     pub required_gender: u32,
     pub item_type: u32,
+    #[serde(default)]
     pub category: u32,
+    #[serde(default)]
     pub members: bool,
+    #[serde(default)]
     pub non_minigame: bool,
+    #[serde(default)]
     pub weapon_trail_effect: u32,
+    #[serde(default)]
     pub composite_effect: u32,
+    #[serde(default)]
     pub power_rating: u32,
+    #[serde(default)]
     pub min_battle_class_level: u32,
+    #[serde(default)]
     pub rarity: u32,
-    pub activatable_ability_id: u32,
-    pub passive_ability_id: u32,
+    #[serde(default)]
     pub single_use: bool,
+    #[serde(default = "default_stack_size")]
     pub max_stack_size: i32,
+    #[serde(default)]
     pub is_tintable: bool,
     pub tint_alias: String,
+    #[serde(default)]
     pub disable_preview: bool,
+    #[serde(default)]
     pub race_set_id: u32,
+    #[serde(default)]
     pub customization_slot: CustomizationSlot,
+    #[serde(default)]
     pub customization_id: u32,
     #[serde(default)]
     pub abilities: Vec<ItemAbilityConfig>,
@@ -103,8 +136,8 @@ impl From<ItemConfig> for ItemDefinition {
             power_rating: cfg.power_rating,
             min_battle_class_level: cfg.min_battle_class_level,
             rarity: cfg.rarity,
-            activatable_ability_id: cfg.activatable_ability_id,
-            passive_ability_id: cfg.passive_ability_id,
+            activatable_ability_id: 0,
+            passive_ability_id: 0,
             single_use: cfg.single_use,
             max_stack_size: cfg.max_stack_size,
             is_tintable: cfg.is_tintable,
