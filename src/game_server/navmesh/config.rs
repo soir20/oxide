b070e7e7eabff5c348b7d8240cb3143455ff3eee
soir20 <71418127+soir20@users.noreply.github.com>
2026-03-26 02:54:39 -0400

    Stitch vertices between layers

--- src/game_server/navmesh/config.rs
+++ src/game_server/navmesh/config.rs
@@ -32,16 +32,57 @@ pub fn load_navmeshes(config_dir: &Path) -> Result<HashMap<String, Navmesh>, Con
     let mut file = File::open(config_dir.join("navmeshes.yaml"))?;
     let config: NavmeshConfig = serde_yaml::from_reader(&mut file)?;
 
+    if config
+        .iter()
+        .any(|(_, layers)| layers.len() > u8::MAX as usize)
+    {
+        return Err(ConfigError::ConstraintViolated(format!(
+            "Cannot have more than {} navmesh layers",
+            u8::MAX
+        )));
+    }
+
     Ok(config
         .into_iter()
         .map(|(asset_name, layers)| {
-            (
-                asset_name,
-                Navmesh::Complex(Mesh {
-                    layers: layers.into_iter().map(Layer::from).collect(),
-                    ..Default::default()
-                }),
-            )
+            let mut layers_by_vertex = HashMap::new();
+            let mut stitch_vertices: HashMap<(u8, u8), Vec<(usize, usize)>> = HashMap::new();
+
+            for (layer_index, layer) in layers.iter().enumerate() {
+                let layer_index = layer_index as u8;
+
+                for (vertex_index, vertex) in layer.outer_edges.iter().enumerate() {
+                    let vertex_bits = [
+                        vertex[0].to_bits(),
+                        vertex[1].to_bits(),
+                        vertex[2].to_bits(),
+                    ];
+
+                    let vertex_layers = layers_by_vertex
+                        .entry(vertex_bits)
+                        .or_insert_with(|| vec![(layer_index, vertex_index)]);
+
+                    if vertex_layers.last().map(|(layer_index, _)| layer_index)
+                        != Some(&layer_index)
+                    {
+                        for (other_layer_index, other_vertex_index) in vertex_layers.iter() {
+                            stitch_vertices
+                                .entry((*other_layer_index, layer_index))
+                                .or_default()
+                                .push((vertex_index, *other_vertex_index));
+                        }
+                    }
+                }
+            }
+
+            let mut mesh = Mesh {
+                layers: layers.into_iter().map(Layer::from).collect(),
+                ..Default::default()
+            };
+
+            mesh.stitch_at_vertices(stitch_vertices.into_iter().collect(), false);
+
+            (asset_name, Navmesh::Complex(mesh))
         })
         .collect())
 }
