d717983ed65753cd36f41b190ed0c5c8ee1d68ae
Delta-07 <166327194+Delta-07@users.noreply.github.com>
2026-09-19 20:50:49 -0500

    Cleanup knock_out()

--- src/game_server/handlers/character.rs
+++ src/game_server/handlers/character.rs
@@ -2465,20 +2465,14 @@ impl CharacterStats {
     }
 
     pub fn knock_out(&self, nearby_player_guids: &[u32]) -> Vec<Broadcast> {
-        let mut broadcasts = Vec::new();
-
         match &self.character_type {
-            CharacterType::AmbientNpc(_) | CharacterType::Fixture(_, _) => {
-                broadcasts.push(Broadcast::Multi(
-                    nearby_player_guids.to_vec(),
-                    self.remove_packets(self.removal_mode),
-                ));
-            }
+            CharacterType::AmbientNpc(_) | CharacterType::Fixture(_, _) => vec![Broadcast::Multi(
+                nearby_player_guids.to_vec(),
+                self.remove_packets(self.removal_mode),
+            )],
             // TODO
-            CharacterType::Player(_) => {}
+            CharacterType::Player(_) => Vec::new(),
         }
-
-        broadcasts
     }
 
     pub fn add_packets(
